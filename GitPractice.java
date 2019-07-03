bb4818e043a1ad4bc1268a16948e812e4646fca6
YuraOvramets <52493047+YuraOvramets@users.noreply.github.com>
2019-07-03 14:24:39 +0300

    Update GitPractice.java

--- GitPractice.java
+++ GitPractice.java
@@ -4,6 +4,6 @@ package lesson4;
 public class GitPractice {
 
     public static void main(String[] args) {
-        System.out.println("This is a first line.");
+        System.out.println("This is a first line. Changed");
     }
 }
