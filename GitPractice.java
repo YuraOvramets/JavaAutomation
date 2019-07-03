49de1be02553ed89377874385c37a9eb3f99a93b
YuraOvramets <52493047+YuraOvramets@users.noreply.github.com>
2019-07-03 14:22:48 +0300

    Update GitPractice.java

--- GitPractice.java
+++ GitPractice.java
@@ -5,5 +5,6 @@ public class GitPractice {
 
     public static void main(String[] args) {
         System.out.println("This is a first line.");
+        System.out.println("This is second line.");
     }
 }
